984bbb7763111dd25157fd8d0fd76b96e5385aff
Thanapat Sarathum <thanapat.sar.tu@gmail.com>
2025-05-25 01:31:31 +0700

    add import asyncio

--- core/consumers.py
+++ core/consumers.py
@@ -4,6 +4,7 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
 from .models import DockerContainer
 import docker
+import asyncio
 
 class MonitoringConsumer(AsyncWebsocketConsumer):
     async def connect(self):
